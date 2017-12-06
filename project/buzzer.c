b232286058149865233384731da7cdc0e12abecb
vrivas28 <vrivas4@miners.utep.edu>
2017-12-05 23:20:52 -0700

    added buzzer

---
+++ project/buzzer.c
@@ -0,0 +1,74 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "buzzer.h"
+
+static long counter = 0;
+static long temp = 0;	
+
+#define MIN_PERIOD 1000
+#define MAX_PERIOD 4000
+
+void buzzer_init()
+{
+    /* 
+       Direct timer A output "TA0.1" to P2.6.  
+        According to table 21 from data sheet:
+          P2SEL2.6, P2SEL2.7, anmd P2SEL.7 must be zero
+          P2SEL.6 must be 1
+        Also: P2.6 direction must be output
+    */
+    timerAUpmode();		/* used to drive speaker */
+    P2SEL2 &= ~(BIT6 | BIT7);
+    P2SEL &= ~BIT7; 
+    P2SEL |= BIT6;
+    P2DIR = BIT6;		/* enable output to speaker (P2.6) */
+
+}
+void play(char choice)
+{
+    switch (choice)
+    {
+        case 1:
+            playBn();
+            break;
+        case 2:
+            playCF();
+            break;
+        case 3: 
+            playE();
+            break;
+    }
+}
+void playBn()
+{
+    temp = 0x222e0;
+    buzzer_set_period(1975);
+    while(++counter < temp){}
+    counter = 0; 
+}
+void playCF()
+{
+    temp = 0x222e0;
+    buzzer_set_period(1108);
+    while(++counter < temp){}
+    counter = 0; 
+}
+
+void playE()
+{
+    temp = 0x222e0;
+    buzzer_set_period(1318);
+    while(++counter < temp){}
+    counter = 0;
+}
+void buzzer_set_period(short cycles)
+{
+  CCR0 = cycles; 
+  CCR1 = cycles >> 1;		/* one half cycle */
+}
+
+
+    
+    
+  
+
---
+++ project/buzzer.h
@@ -0,0 +1,11 @@
+#ifndef buzzer_included
+#define buzzer_included
+
+void buzzer_init();
+void play(char choice);
+void playBn();
+void playE();
+void playCF();
+void buzzer_set_period(short cycles);
+
+#endif // included
